916a264fb45855a4708f8f2dafaa57b1f774cb71
hitoshi <hitoshi@hitoshi-PC.(none)>
2012-05-21 08:20:06 +0900

    setting rprompt git branches

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -1,38 +1,21 @@
 # version  controll system
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' formats ':(%s)%b'
-zstyle ':vcs_info:*' actionformats ':(%s)%b|%a'
+zstyle ':vcs_info:*' enable git svn hg bzr
+zstyle ':vcs_info:*' formats '(%s)-[%b]'
+zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
+zstyle ':vcs_info:(svn|bzr):*' branchformat '%b:r%r'
+zstyle ':vcs_info:bzr:*' use-simple true
+
 precmd () {
     psvar=()
     LANG=en_US.UTF-8 vcs_info
     [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
 
-# prompt colors
-case ${UID} in
-0)
-    PROMPT="%B%{[31m%}%/#%{[m%}%b "
-    PROMPT2="%B%{[31m%}%_#%{[m%}%b "
-    SPROMPT="%B%{[31m%}%r is correct? [n,y,a,e]:%{[m%}%b "
-    [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
-        PROMPT="%{[37m%}${HOST%%.*} ${PROMPT}"
-    ;;
-*)
-    # PROMPT="%{${fg[blue]}%}[%n@%m] %(!.#.$) %{${reset_color}%} "
-echo "hhhheeeello"
-    PROMPT="%{${fg[yellow]}%}[%n@%m%1(v|%F{green}%1v%f|)%{${fg[yellow]}%}]%{${reset_color}%}%b "
-    PROMPT2="%{${fg[blue]}%}%_> %{${reset_color}%}"
-    SPROMPT="%{${fg[red]}%}correct: %R -> %r [nyae]? %{${reset_color}%}"
-    RPROMPT="%{${fg[yellow]}%}[%~]%{${reset_color}%}  "
-    ;;
-esac
+PROMPT="%{${fg[blue]}%}[%n@%m] %{${reset_color}%}%(!.#.$) "
+PROMPT2="%{${fg[blue]}%}%_> %{${reset_color}%}"
+SPROMPT="%{${fg[red]}%}correct: %R -> %r [nyae]? %{${reset_color}%}"
+RPROMPT="%1(v|%F{red}%1v%f|) %{${fg[yellow]}%}[%~]%{${reset_color}%}  "
 
-case "${TERM}" in
-    kterm*|xterm)
-        precmd() {
-            echo -ne "\033]0;${USER}@${HOST%%.*}:${PWD}\007"
-        }
-        ;;
-esac
 
 
